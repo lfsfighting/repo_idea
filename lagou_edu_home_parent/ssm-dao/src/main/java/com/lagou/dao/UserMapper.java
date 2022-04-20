6fbfde7a1bfb1845950535ddcfa8896cc2515062
lfsfighting <74913712+lfsfighting@users.noreply.github.com>
2022-04-20 20:18:50 +0800

    Update UserMapper.java

--- lagou_edu_home_parent/ssm-dao/src/main/java/com/lagou/dao/UserMapper.java
+++ lagou_edu_home_parent/ssm-dao/src/main/java/com/lagou/dao/UserMapper.java
@@ -46,4 +46,8 @@ public interface UserMapper {
         4.获取用户拥有的资源权限信息
      */
     public List<Resource> findResourceByRoleId(List<Integer> ids);
+    
+    4.获取用户拥有的资源权限信息
+     */
+    public List<Resource> findResourceByRoleId2(List<Integer> ids);
 }
